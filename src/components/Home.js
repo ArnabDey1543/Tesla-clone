32b52972f1d16d2d4939e497967c88981c4c9ac0
ArnabDey1543 <arnabdey1543@gmail.com>
2022-03-26 16:19:31 +0530

    Initial Commit of master class

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import style from "styled-components"
+
+function Home() {
+  return (
+    <container>
+        Home
+    </container>
+  )
+}
+
+export default Home
+
+const container = styled.div`
+    height: 100vh;
+`
---
+++ src/components/Section.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function section() {
+  return (
+    <div>section</div>
+  )
+}
+
+export default section
