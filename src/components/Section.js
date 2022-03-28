55c755f8535aff16aaa66de9ee144180c91d930a
ArnabDey1543 <arnabdey1543@gmail.com>
2022-03-28 11:49:31 +0530

    created section component

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,19 +1,57 @@
-import React from 'react'
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
 function Section() {
   return (
-    <Wrap>Section</Wrap>
-  )
+    <Wrap>
+      <ItemText>
+        <h1>Model s</h1>
+        <p>Order Online for Touchless Delivery</p>
+      </ItemText>
+      <ButtonGroup>
+        <LeftButton>
+          Custom Oder  
+        </LeftButton>
+        <RightButton>
+          Existing Inventory
+        </RightButton>
+
+      </ButtonGroup>
+    </Wrap>
+  );
 }
 
-export default Section
+export default Section;
 
 const Wrap = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background: oragne;
-    background-image: url('/images/model-s.jpg');
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  // background: orange;
+  background-image: url("/images/model-s.jpg");
+`
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+
+
+`
+const ButtonGroup = styled.div`
+
+
+
+`
+
+
+const LeftButton = styled.div`
+
+
+`
+
+const RightButton = styled.div`
 
 
 `
