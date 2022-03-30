0e7e4bef7386e6c129df44f85ba87743949f37c6
ArnabDey1543 <arnabdey1543@gmail.com>
2022-03-30 13:11:36 +0530

    addded downarrow animation

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,15 +8,14 @@ function Section() {
         <h1>Model s</h1>
         <p>Order Online for Touchless Delivery</p>
       </ItemText>
-      <ButtonGroup>
-        <LeftButton>
-          Custom Oder  
-        </LeftButton>
-        <RightButton>
-          Existing Inventory
-        </RightButton>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>Custom Oder</LeftButton>
+          <RightButton>Existing Inventory</RightButton>
+        </ButtonGroup>
 
-      </ButtonGroup>
+        <DownArrow src="./images/down-arrow.svg" />
+      </Buttons>
     </Wrap>
   );
 }
@@ -31,27 +30,45 @@ const Wrap = styled.div`
   background-repeat: no-repeat;
   // background: orange;
   background-image: url("/images/model-s.jpg");
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; //vertical
+  align-items: center; //horizontal
+`;
 
 const ItemText = styled.div`
   padding-top: 15vh;
   text-align: center;
-
-
-`
+`;
 const ButtonGroup = styled.div`
-  
-
-
-`
-
+  display: flex;
+  margin-bottom: 30px;
+`;
 
 const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
 
+const RightButton = styled(LeftButton)``;
 
-`
+const DownArrow = styled.img`
+  height: 40px;
+  overflow-x: hidden;
+  animation: animateDown infinite 1.5s;
+`;
 
-const RightButton = styled.div`
+const Buttons = styled.div`
 
-
-`
+`;
