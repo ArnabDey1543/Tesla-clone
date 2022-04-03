37d2ef832cedcf09e71f3e95a1f1cc362b1b458d
ArnabDey1543 <arnabdey1543@gmail.com>
2022-04-03 21:34:31 +0530

    Header tab added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,62 @@
 import React from 'react'
+import styled from "styled-components"
 
 function Header() {
   return (
-    <div>Header</div>
+    <Container>
+        <a>
+          <img src="/images/logo.svg" alt="" />
+        </a>
+        <Menu> 
+          <a href="#">Model S</a>
+          <a href="#">Model 3</a>
+          <a href="#">Model X</a>
+          <a href="#">Model X</a>
+        </Menu>
+        <RightMenu>
+          <a href="#" >Shop</a>
+          <a href="#" >Tesla Account</a>
+
+
+        </RightMenu>
+    </Container>
   )
 }
 
 export default Header
+
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0; //making the header full width
+
+`
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  a{
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+`
+
+const RightMenu = styled.div`
+a{
+  font-weight: 600;
+  text-transform: uppercase;
+  padding: 0 10px;
+  flex-wrap: nowrap;
+}
+
+`
