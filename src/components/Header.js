50bb771d52623153695328e1e8a6cc47f2a1c57a
ArnabDey1543 <arnabdey1543@gmail.com>
2022-04-07 19:10:40 +0530

    sidebar changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,65 @@
-import React from 'react'
-import styled from "styled-components"
-import MenuIcon from '@material-ui/icons/Menu';
-
+import React from "react";
+import styled from "styled-components";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
 
 function Header() {
   return (
     <Container>
-        <a>
-          <img src="/images/logo.svg" alt="" />
-        </a>
-        <Menu> 
-          <a href="#">Model S</a>
-          <a href="#">Model 3</a>
-          <a href="#">Model X</a>
-          <a href="#">Model X</a>
-        </Menu>
-        <RightMenu>
-          <a href="#" >Shop</a>
-          <a href="#" >Tesla Account</a>
-          <CustomMenu />
-        </RightMenu>
-        <BurgerNav>
-            <li><a href='#'>Existing Iventory</a></li>
-            <li><a href='#'>Used Inventory</a></li>
-            <li><a href='#'>Trade-in</a></li>
-            <li><a href='#'>Cybertruck</a></li>
-            <li><a href='#'>Roadster</a></li>
-            <li><a href='#'>Semi</a></li>
-            <li><a href='#'>Charging</a></li>
-            <li><a href='#'>Power</a></li>
-            <li><a href='#'>Utilities</a></li>
-            <li><a href='#'>Test Drive</a></li>
-        </BurgerNav>
-
+      <a>
+        <img src="/images/logo.svg" alt="" />
+      </a>
+      <Menu>
+        <a href="#">Model S</a>
+        <a href="#">Model 3</a>
+        <a href="#">Model X</a>
+        <a href="#">Model X</a>
+      </Menu>
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+        <CustomMenu />
+      </RightMenu>
+      <BurgerNav>
+        <CloseWrapper>
+          <CustomClose />
+        </CloseWrapper>
+        <li>
+          <a href="#">Existing Iventory</a>
+        </li>
+        <li>
+          <a href="#">Used Inventory</a>
+        </li>
+        <li>
+          <a href="#">Trade-in</a>
+        </li>
+        <li>
+          <a href="#">Cybertruck</a>
+        </li>
+        <li>
+          <a href="#">Roadster</a>
+        </li>
+        <li>
+          <a href="#">Semi</a>
+        </li>
+        <li>
+          <a href="#">Charging</a>
+        </li>
+        <li>
+          <a href="#">Power</a>
+        </li>
+        <li>
+          <a href="#">Utilities</a>
+        </li>
+        <li>
+          <a href="#">Test Drive</a>
+        </li>
+      </BurgerNav>
     </Container>
-  )
+  );
 }
 
-export default Header
-
+export default Header;
 
 const Container = styled.div`
   min-height: 60px;
@@ -51,42 +72,38 @@ const Container = styled.div`
   left: 0;
   right: 0; //making the header full width
   z-index: 1;
-`
+`;
 
 const Menu = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   flex: 1;
-  a{
+  a {
     font-weight: 600;
     text-transform: uppercase;
     padding: 0 10px;
     flex-wrap: nowrap;
   }
 
-  @media(max-width: 768){
+  @media (max-width: 768) {
     display: none;
   }
-`
- 
+`;
+
 const RightMenu = styled.div`
-  display:felx;
-  align-items:center;
-a{
-  font-weight: 600;
-  text-transform: uppercase;
-  margin: 10px
+  display: felx;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin: 10px;
+  }
+`;
 
-}
-
-`
-
-const CustomMenu =styled(MenuIcon)`
-  cursor : pointer;
-
-
-`
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`;
 
 const BurgerNav = styled.div`
   position: fixed;
@@ -96,4 +113,25 @@ const BurgerNav = styled.div`
   background: white;
   width: 300px;
   z-index: 16;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+
+    a {
+      font-weight: 600;
+    }
+  }
+`;
+
+const CustomClose = styled(CloseIcon)``;
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+
 `
